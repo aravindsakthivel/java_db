a1104359184315970d1ad63d39f2858cb17fb4fc
aravindsakthivel <aravind.r.sakthi@gmail.com>
2024-02-11 20:22:29 +0530

    JDBC connect success

---
+++ src/com/masai/Main.java
@@ -0,0 +1,32 @@
+package com.masai;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            System.out.println("Hi");
+            e.printStackTrace();
+        }
+
+        String DbUrl = "jdbc:mysql://localhost:3306/learn_jdbc";
+        try{
+            Connection conn = DriverManager.getConnection(DbUrl, "root", "root");
+
+            if(conn != null){
+                System.out.println("Connected");
+            }else {
+                System.out.println("Not connected");
+            }
+        } catch(SQLException e){
+            e.printStackTrace();
+        }
+
+    }
+
+}
